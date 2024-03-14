fe00a7cc915b1921f394b41d3e5fc5cd315f9999
lifedesign2001 <163286669+lifedesign2001@users.noreply.github.com>
2024-03-14 12:04:46 +0700

    Update next.config.mjs
    export static

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /*output: 'export',
-    basePath: '/past-experience/showcase/roll_dice_next',*/
+    output: 'export',
+    /*basePath: '/past-experience/showcase/roll_dice_next',*/
 };
 
 export default nextConfig;
