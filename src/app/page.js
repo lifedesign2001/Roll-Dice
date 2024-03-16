5d3d5f897d4f2fa603a5170fe57154aa798befd6
lifedesign2001 <163286669+lifedesign2001@users.noreply.github.com>
2024-03-17 00:31:52 +0700

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -103,16 +103,16 @@ export default function Home() {
           document.getElementById('diceItem'+i).style.transform = "rotateX(180deg) rotateY(0deg)";
         } else if  (diceval == 2) {
           //document.getElementById('diceItem'+i).style = { "transform" : "rotateX(-91deg) rotateY(1deg)"};
-          document.getElementById('diceItem'+i).style.transform = "rotateX(-91deg) rotateY(1deg)";
+          document.getElementById('diceItem'+i).style.transform = "rotateX(-90deg) rotateY(0deg)";
         } else if  (diceval == 5) {
           //document.getElementById('diceItem'+i).style = { "transform" : "rotateX(91deg) rotateY(1deg)"};
-          document.getElementById('diceItem'+i).style.transform = "rotateX(91deg) rotateY(1deg)";
+          document.getElementById('diceItem'+i).style.transform = "rotateX(90deg) rotateY(0deg)";
         } else if  (diceval == 3) {
           //document.getElementById('diceItem'+i).style = { "transform" : "rotateX(1deg) rotateY(91deg)"};
-          document.getElementById('diceItem'+i).style.transform = "rotateX(1deg) rotateY(91deg)";
+          document.getElementById('diceItem'+i).style.transform = "rotateX(0deg) rotateY(90deg)";
         } else if  (diceval == 4) {
           //document.getElementById('diceItem'+i).style = { "transform" : "rotateX(1deg) rotateY(-91deg)"};
-          document.getElementById('diceItem'+i).style.transform = "rotateX(1deg) rotateY(-91deg)";
+          document.getElementById('diceItem'+i).style.transform = "rotateX(0deg) rotateY(-90deg)";
         }
 
         if (i == (diceList.length - 1)) {
